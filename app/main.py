31ddcf49dde6cb56b0feb9aeccfea3c17aca2379
diegombtavares <diego.mborges08@gmail.com>
2024-02-04 16:25:47 -0300

    aplicação de html

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God Of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS4')
+lista = [jogo1, jogo2, jogo3]
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+app.run(debug=True)
